fd99dddc185b2d8153e51a4ca8209b8b54d8385c
Walter Tamboer <walter@tamboer.nl>
2022-03-30 20:35:02 +0200

    Added the building of Docker files

---
+++ .docker/php-fpm/usr/local/bin/dev-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Stop on errors
+set -e
+
+_VHOST_DIR=/opt/phpsupport
+
+# Set the apache user and group to match the host user.
+OWNER=$(stat -c '%u' "$_VHOST_DIR")
+GROUP=$(stat -c '%g' "$_VHOST_DIR")
+
+if [[ "$OWNER" != "0" ]]; then
+	sed -i "s/:82:82:/:${OWNER}:${GROUP}:/" /etc/passwd 
+	sed -i "s/:82:/:${GROUP}:/" /etc/group
+fi
+
+exec "$@"
